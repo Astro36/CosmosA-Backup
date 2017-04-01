3aee508a1a06a6fbbffeb0c608f00987a5702c48
Astro36 <astr36@naver.com>
2017-04-01 23:13:12 +0900

    Rename MineDev to Cosmos A

--- index.php
+++ index.php
@@ -13,12 +13,17 @@
     <div class="mdl-color--grey-100 mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <header class="mdl-layout__header mdl-layout__header--waterfall">
             <div class="mdl-layout__header-row">
-                <span class="mdl-layout-title">MineDev</span>
+                <span class="mdl-layout-title">Cosmos A</span>
             </div>
-            <span class="" style="margin: 16px; margin-left: 72px;"><a class="mdl-color-text--white" href="javascript:scrollTo('#about', 120)" style="text-decoration: none;">About</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;<a class="mdl-color-text--white" href="javascript:scrollTo('#astro', 120)" style="text-decoration: none;">Astro</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;<a class="mdl-color-text--white" href="javascript:scrollTo('#meta', 120)" style="text-decoration: none;">Meta</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;<a class="mdl-color-text--white" href="javascript:scrollTo('#cosmosa', 120)" style="text-decoration: none;">Cosmos A&nbsp;&nbsp;&gt;&nbsp;&nbsp;<a class="mdl-color-text--white" href="javascript:scrollTo('#apps', 108)" style="text-decoration: none;">Apps</a></span>
+            <span class="" style="margin: 16px; margin-left: 72px;">
+                <a class="mdl-color-text--white" href="javascript:scrollTo('#about', 120)" style="text-decoration: none;">About</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;
+                <a class="mdl-color-text--white" href="javascript:scrollTo('#astro', 120)" style="text-decoration: none;">Astro</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;
+                <a class="mdl-color-text--white" href="javascript:scrollTo('#meta', 120)" style="text-decoration: none;">Meta</a>&nbsp;&nbsp;&gt;&nbsp;&nbsp;
+                <a class="mdl-color-text--white" href="javascript:scrollTo('#apps', 108)" style="text-decoration: none;">Apps</a>
+            </span>
         </header>
         <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">MineDev</span>
+            <span class="mdl-layout-title">Cosmos A</span>
             <nav class="mdl-navigation">
                 <?php
                     $navs = json_decode(file_get_contents('navs.json'), true);
@@ -33,7 +38,7 @@
                 <table style="margin-left: 10%; margin-right: 10%;">
                     <tr style="height: 168px;">
                         <td style="margin: 0 auto; width: 128px;">
-                            <img src="assets/ic_create_white.png" width="96px" height="96px">
+                            <img src="assets/ic_cube_outline_white.png" width="96px" height="96px">
                         </td>
                         <td class="mdl-color-text--white">
                             <h4 style="margin-top: 0;">Index</h4>
@@ -45,7 +50,7 @@
             </div>
             <div class="mdl-grid">
                 <div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--8-col mdl-cell--2-offset-desktop" id="about" style="padding: 24px 24px 24px 24px">
-                    <div style="background: url('assets/img_favicon.png') center / cover; border-radius: 8px; margin: 0 auto; width: 200px; height: 200px;">
+                    <div style="background: url('assets/img_cosmosa.png') center / cover; border-radius: 8px; margin: 0 auto; width: 200px; height: 200px;">
                     </div>
 
                     <h4>About</h4>
@@ -78,21 +83,14 @@
                     <h4>Meta</h4>
                     Meta는 2016년 청소년 개발자를 주축으로 설립되었으며, Team IO, Object Studio, Team LeveL의 역사를 이어받고 있습니다.
                 </div>
-                <div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--8-col mdl-cell--2-offset-desktop" id="cosmosa" style="padding: 24px 24px 24px 24px">
-                    <div style="background: url('assets/img_cosmosa.png') center / cover; border-radius: 8px; margin: 0 auto; width: 200px; height: 200px;">
-                    </div>
-
-                    <h4>Cosmos A</h4>
-                    Coming Soon!
-                </div>
             </div>
 
             <div style="background: url('assets/img_full_moon.jpg') center / cover; margin-top: 24px;">
                 <h1 style="margin: 0;"><br><br><br><br></h1>
             </div>
-            <div class="mdl-color--blue-grey-700" style="margin-bottom: 24px; padding: 28px; text-align: center;">
+            <div class="mdl-color--primary" style="margin-bottom: 24px; padding: 28px; text-align: center;">
                 <h2 class="mdl-color-text--white">Hello, world!</h2>
-                <h4 class="mdl-color-text--white">Welcome to MineDev</h4>
+                <h4 class="mdl-color-text--white">Welcome to Cosmos A</h4>
             </div>
 
             <div class="mdl-grid" id="apps">
